003d9606322a35b7653f0caf82ce8a5bdcf7046a
sippin <regulationqq@mail.ru>
2025-03-16 21:05:07 +0300

    visitblock edit

--- app/components/VisitBlock.jsx
+++ app/components/VisitBlock.jsx
@@ -6,7 +6,7 @@ export default function VisitBlock() {
 
         <div className='farm_container section flex justify-between '>
             <div className='flex flex-col justify-center'>
-                <h1 className='visit_title text-[48px] text-[#F7C35F] font-bold'>Предлагаем пшеницу и лен</h1>
+                <h1 className='visit_title text-[48px] text-[#F7C35F] font-bold'>Предлагаем пшеницу</h1>
                 <span className='visit_subtitle text-[36px] text-[#EADCC1] font-bold'>От фермерского хозяйства</span>
                 <button className='visit_button text-[18px] bg-[#F7C35F] rounded-[15px] w-fit px-[25px] py-[10px] mt-[50px]'><a href="#contact">Оставить запрос</a></button>
             </div>
