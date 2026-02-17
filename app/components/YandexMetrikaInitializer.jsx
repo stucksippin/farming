87d78f6142ced44564d9c048ef68f21a6bdddda0
sippin <regulationqq@mail.ru>
2026-02-17 10:47:38 +0300

    fixed metrika counter

--- app/components/YandexMetrikaInitializer.jsx
+++ app/components/YandexMetrikaInitializer.jsx
@@ -1,43 +1,29 @@
-// app/components/YandexMetrika.jsx
 "use client";
 
 import Script from "next/script";
 
-const METRIKA_ID = 12345678; // ← замени на свой ID
-
 export default function YandexMetrika() {
     return (
-        <>
-            <Script id="yandex-metrika" strategy="afterInteractive">
-                {`
-          (function(m,e,t,r,i,k,a){
-            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {
-              if (document.scripts[j].src === r) { return; }
-            }
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-            k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        <Script id="yandex-metrika" strategy="afterInteractive">
+            {`
+                (function(m,e,t,r,i,k,a){
+                    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104766528', 'ym');
 
-          ym(${METRIKA_ID}, "init", {
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true
-          });
-        `}
-            </Script>
-
-            <noscript>
-                <div>
-                    <img
-                        src={`https://mc.yandex.ru/watch/${METRIKA_ID}`}
-                        style={{ position: "absolute", left: "-9999px" }}
-                        alt=""
-                    />
-                </div>
-            </noscript>
-        </>
+                ym(104766528, 'init', {
+                    ssr: true,
+                    webvisor: true,
+                    clickmap: true,
+                    ecommerce: "dataLayer",
+                    referrer: document.referrer,
+                    url: location.href,
+                    accurateTrackBounce: true,
+                    trackLinks: true
+                });
+            `}
+        </Script>
     );
 }
